384c53a8f82098f007917df04528f76020026250
Ionut Birlad <birladionut@yahoo.it>
2020-05-05 15:59:40 +0200

    progresso

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,82 @@
+<footer>
+  <div class="container">
+    <section>
+      <img id="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="logo molisana">
+      <div class="contacts">
+        <ul>
+          <li>Ragione sociale: La Molisana S.P.A.</li>
+          <li>Sede legale: Contrada Colle delle Api, 100/A <br> 86100 - Campobasso (CB)</li>
+          <li>Pec: lamolisana@pec.it</li>
+          <li>Tel: +39 0874 4981</li>
+          <li>Fax: +39 0874 629584</li>
+          <li> <a href="#">info@lamolisana.it</a> <p>(per segnalazioni degli utenti)</p> </li>
+          <li> <a href="#">commerciale@lamolisana.it</a> </li>
+          <li> <a href="#">export@lamolisana.it</a> </li>
+          <li> <p>Dati aziendali:</p> <a href="#">leggi di più</a> </li>
+        </ul>
+      </div>
+      <div class="socials">
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter-square"></i>
+        <i class="fab fa-youtube-square"></i>
+      </div>
+      <img src="{{asset('img/numero-verde.jpg')}}" alt="">
+      <img src="{{asset('img/whatsapp.jpg')}}" alt="">
+    </section>
+    <section>
+      <div class="upper">
+        <h3>PASTIFICIO</h3>
+        <ul>
+          <li> <a href="#">Il Pastificio</a> </li>
+          <li> <a href="#">Grano decorticato a pietra</a> </li>
+          <li> <a href="#">Il Molise c’è</a> </li>
+          <li> <a href="#">Filiera Integrata</a> </li>
+          <li> <a href="#">100 anni di pasta</a> </li>
+          <li> <a href="#">Sartoria della pasta</a> </li>
+          <li> <a href="#">Spaghetto Quadrato</a> </li>
+          <li> <a href="#">Le Persone</a> </li>
+        </ul>
+      </div>
+      <div class="lower">
+        <h3>PRODOTTI</h3>
+        <ul>
+          <li> <a href="#">Le Classiche</a> </li>
+          <li> <a href="#">Le Integrali</a> </li>
+          <li> <a href="#">Le Speciali</a> </li>
+          <li> <a href="#">Le Biologiche</a> </li>
+          <li> <a href="#">Le Gluten-Free</a> </li>
+          <li> <a href="#">Le Semole</a> </li>
+          <li> <a href="#">Le Extra di Lusso</a> </li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div class="upper">
+        <h3>COLLEZIONE DA CHEF</h3>
+        <ul>
+          <li> <a href="#">Collezione da Chef</a> </li>
+          <li> <a href="#">Grandi Cucine</a> </li>
+          <li> <a href="#">Biologiche</a> </li>
+          <li> <a href="#">Quadrate</a> </li>
+        </ul>
+      </div>
+      <div class="lower">
+        <h3>LIMITED EDITION NEWS</h3>
+        <ul>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> E-COMMERCE</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> B2B</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> COMUNICAZIONE</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> SCARICA I CATALOGHI</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> RASSEGNA STAMPA</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> VIDEO</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> CONTATTI</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> LAVORA CON NOI</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> CERTIFICAZIONI</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> PRIVACY POLICY</a> </li>
+          <li> <a href="#"> <i class="fas fa-chevron-right"></i> CREDITS</a> </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <img id="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="logo molisana">
+    </div>
+    <div class="row">
+      <nav id="menu">
+        <ul>
+          <li> <a href="#">Home</a> </li>
+          <li> <a href="#">Prodotti</a> </li>
+          <li> <a href="#">News</a> </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
