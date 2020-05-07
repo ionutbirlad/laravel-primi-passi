b8e2f67e9298db5512d5132132e8c1179c2d27d2
Ionut Birlad <birladionut@yahoo.it>
2020-05-07 16:07:40 +0200

    aggiunto active alle voci della navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,9 +6,9 @@
     <div class="row">
       <nav id="menu">
         <ul>
-          <li> <a href="{{route('home')}}">Home</a> </li>
-          <li> <a href="{{route('prodotti')}}">Prodotti</a> </li>
-          <li> <a href="{{route('contatti')}}">Contatti</a> </li>
+          <li class="{{(url() -> current() == route('home')) ? 'active' : ''}}"> <a href="{{route('home')}}">Home</a> </li>
+          <li class="{{(url() -> current() == route('prodotti')) ? 'active' : ''}}"> <a href="{{route('prodotti')}}">Prodotti</a> </li>
+          <li class="{{(url() -> current() == route('contatti')) ? 'active' : ''}}"> <a href="{{route('contatti')}}">Contatti</a> </li>
         </ul>
       </nav>
     </div>
